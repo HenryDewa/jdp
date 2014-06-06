684fdd51e17c88770542d0289f2315b3682dfcfc
Kenold Beauplan <bkenold@gmail.com>
2014-06-06 14:15:59 -0400

    initial commit

---
+++ app/views/site/about.blade.php
@@ -0,0 +1,14 @@
+@extends('site/layouts/default')
+
+{{-- Page title --}}
+@section('title')
+About us ::
+@parent
+@stop
+
+{{-- Page content --}}
+@section('content')
+
+<h1>About JD Productions.</h1>
+
+@stop
